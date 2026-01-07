fb303c866dd67b8074294d71defed58a703872c6
DonotFreeze <59327236+DonotFreeze@users.noreply.github.com>
2026-01-07 17:44:09 +0800

    ``` feat: 初始化G431_COT_DEMO项目配置
    添加了项目的基础配置文件，包括：
    - .clang-format: 配置C++代码格式化规则
    - .eide/eide.yml: EIDE项目配置文件
    - .eide/files.options.yml: 编译器选项配置
    - .gitignore: Git忽略文件配置
    - .mxproject: MicroXplorer项目文件
    - .vscode/settings.json 和 tasks.json: VSCode开发环境配置
    - Core/Inc目录下的头文件：adc.h, comp.h, dac.h, dma.h, fdcan.h,
      gpio.h, main.h, opamp.h等外设初始化头文件
    ```

---
+++ Core/Inc/main.h
@@ -0,0 +1,109 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32g4xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define LED_BLUE_Pin GPIO_PIN_13
+#define LED_BLUE_GPIO_Port GPIOC
+#define LED_GREEN_Pin GPIO_PIN_14
+#define LED_GREEN_GPIO_Port GPIOC
+#define LED_RED_Pin GPIO_PIN_15
+#define LED_RED_GPIO_Port GPIOC
+#define I_MOS_Pin GPIO_PIN_0
+#define I_MOS_GPIO_Port GPIOA
+#define I_BAT_Pin GPIO_PIN_1
+#define I_BAT_GPIO_Port GPIOA
+#define REF1V65_OUT_Pin GPIO_PIN_2
+#define REF1V65_OUT_GPIO_Port GPIOA
+#define REF1V65_IN_Pin GPIO_PIN_3
+#define REF1V65_IN_GPIO_Port GPIOA
+#define V_BAT_Pin GPIO_PIN_4
+#define V_BAT_GPIO_Port GPIOA
+#define VCAP_Pin GPIO_PIN_2
+#define VCAP_GPIO_Port GPIOB
+#define TEMP_MOS_Pin GPIO_PIN_11
+#define TEMP_MOS_GPIO_Port GPIOB
+#define BOOM_IN_Pin GPIO_PIN_13
+#define BOOM_IN_GPIO_Port GPIOB
+#define TEMP_CAP_Pin GPIO_PIN_15
+#define TEMP_CAP_GPIO_Port GPIOB
+#define ENABLE_CAP_Pin GPIO_PIN_5
+#define ENABLE_CAP_GPIO_Port GPIOB
+
+/* USER CODE BEGIN Private defines */
+
+#define LED_RED_ON HAL_GPIO_WritePin(LED_RED_GPIO_Port,LED_RED_Pin,GPIO_PIN_SET)
+#define LED_RED_OFF HAL_GPIO_WritePin(LED_RED_GPIO_Port,LED_RED_Pin,GPIO_PIN_RESET)
+#define LED_RED_BLINK HAL_GPIO_TogglePin(LED_RED_GPIO_Port,LED_RED_Pin)
+
+#define LED_GREEN_ON HAL_GPIO_WritePin(LED_GREEN_GPIO_Port,LED_GREEN_Pin,GPIO_PIN_SET)
+#define LED_GREEN_OFF HAL_GPIO_WritePin(LED_GREEN_GPIO_Port,LED_GREEN_Pin,GPIO_PIN_RESET)
+#define LED_GREEN_BLINK HAL_GPIO_TogglePin(LED_GREEN_GPIO_Port,LED_GREEN_Pin)
+
+#define LED_BLUE_ON HAL_GPIO_WritePin(LED_BLUE_GPIO_Port,LED_BLUE_Pin,GPIO_PIN_SET)
+#define LED_BLUE_OFF HAL_GPIO_WritePin(LED_BLUE_GPIO_Port,LED_BLUE_Pin,GPIO_PIN_RESET)
+#define LED_BLUE_BLINK HAL_GPIO_TogglePin(LED_BLUE_GPIO_Port,LED_BLUE_Pin)
+
+
+#define SET_PWM_COMPARE(a) htim1.Instance->CCR2 = (a)
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
